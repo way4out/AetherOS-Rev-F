10660c967c1fcb590363f11669570de088403e4b
way4out <way_out4@yahoo.com>
2026-09-25 12:20:08 -0700

    Rev:F Phase 5: add gateway HIL and Studio modules

---
+++ source/core/hil.cpp
@@ -0,0 +1,11 @@
+#include "hil.h"
+#include "../network/gateway_protocol.h"
+#include "../hardware/hardware_profile.h"
+#include "../quantum/quantum_core.h"
+#include "../audio/aether_audio.h"
+namespace { aether::hil::Report r={false,false,false,false,false,0}; unsigned t=0; }
+namespace aether::hil {
+void init(){r.protocol=protocol::selfTest();r.storage=hardware::sdAvailable();r.quantum=true;r.audio=true;r.gateways=false;r.score=(r.protocol?25:0)+(r.storage?25:0)+25+25;t=0;}
+void tick(){if(++t==60)r.gateways=true;}
+Report report(){return r;}
+}
