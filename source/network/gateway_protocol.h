6ac3bd0155c24b5bc29346822f1cd733efb5929c
way4out <way_out4@yahoo.com>
2026-09-25 02:14:10 -0700

    Pass 2 protocol layer

---
+++ source/network/gateway_protocol.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <nds.h>
+namespace aether::protocol {
+enum Kind { HELLO=1, HELLO_ACK=2, PING=3, PONG=4, AUTH=5, JOB=6, RESULT=7, TELEMETRY=8, QUEUE=9 };
+struct Frame { u16 version; u16 kind; u32 id; u32 length; u32 crc; };
+u32 crc32(const void*, u32);
+bool encode(Kind, u32, const void*, u32, void*, u32, u32&);
+bool decode(const void*, u32, Frame&, const void*&);
+}
