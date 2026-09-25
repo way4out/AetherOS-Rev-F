717b56802771c482d4bdbf352a768daf44623c6b
way4out <way_out4@yahoo.com>
2026-09-25 02:31:41 -0700

    Rumble #9 align Wi-Fi receive length ABI

--- source/network/wifi_transport.h
+++ source/network/wifi_transport.h
@@ -4,5 +4,5 @@ bool init();
 bool connected();
 unsigned ip();
 bool httpGet(const char* host,const char* path,char* out,unsigned cap);
-bool tcpExchange(const char* host,unsigned port,const void* tx,unsigned txLen,void* rx,unsigned rxCap,unsigned& rxLen);
+bool tcpExchange(const char* host,unsigned port,const void* tx,unsigned txLen,void* rx,unsigned rxCap,u32& rxLen);
 }
