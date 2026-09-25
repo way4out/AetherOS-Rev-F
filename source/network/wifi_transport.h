1016c59df79fc6236452541cdfd61b0a7bd46077
way4out <way_out4@yahoo.com>
2026-09-25 02:15:50 -0700

    Pass 2 live Wi-Fi gateway transport

---
+++ source/network/wifi_transport.h
@@ -0,0 +1,8 @@
+#pragma once
+namespace aether::wifi {
+bool init();
+bool connected();
+unsigned ip();
+bool httpGet(const char* host,const char* path,char* out,unsigned cap);
+bool tcpExchange(const char* host,unsigned port,const void* tx,unsigned txLen,void* rx,unsigned rxCap,unsigned& rxLen);
+}
