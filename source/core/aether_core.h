92ef0327c1bdec057899fa07432dd7fa487c4b24
way4out <way_out4@yahoo.com>
2026-09-24 21:22:39 -0700

    Build Rev F interactive core and SD workspace

--- source/core/aether_core.h
+++ source/core/aether_core.h
@@ -1,14 +1,19 @@
 #pragma once
 #include <nds.h>
+
 namespace aether {
 struct SystemState {
     bool sdReady;
     bool touchReady;
     bool benchmarkComplete;
     bool safeMode;
+    bool sdWriteReady;
     u32 frame;
+    int selectedModule;
 };
+
 void init(SystemState& state);
 void update(SystemState& state);
 void render(const SystemState& state);
+void shutdown();
 }
