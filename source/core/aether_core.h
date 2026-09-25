71967ff10d7683829ddc826b9684072f5a59e9c0
way4out <way_out4@yahoo.com>
2026-09-24 21:26:47 -0700

    Rev:F: integrate interactive desktop and runtime modules

--- source/core/aether_core.h
+++ source/core/aether_core.h
@@ -1,23 +1,15 @@
 #pragma once
 #include <nds.h>
-
+#include "../quantum/quantum_core.h"
 namespace aether {
 struct SystemState {
-    bool sdReady;
-    bool touchReady;
-    bool benchmarkComplete;
-    bool safeMode;
-    bool sdWriteReady;
-    bool audioReady;
-    bool quantumReady;
-    bool projectSaved;
-    u32 frame;
-    int selectedModule;
-    int screen;
+    bool sdReady; bool touchReady; bool benchmarkComplete; bool safeMode;
+    bool sdWriteReady; bool audioReady; bool quantumReady; bool projectSaved;
+    u32 frame; int selectedModule; int screen;
 };
-
-void init(SystemState& state);
-void update(SystemState& state);
-void render(const SystemState& state);
+void init(SystemState&);
+void update(SystemState&);
+void render(const SystemState&);
 void shutdown();
+quantum::Simulator& simulator();
 }
