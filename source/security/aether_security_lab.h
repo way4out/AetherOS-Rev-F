6a3b6ea334b834d2c079d7658d2c9833660e5d1c
way4out <way_out4@yahoo.com>
2026-09-25 15:20:15 -0700

    Rev7 Pass 2: isolate security lab namespace from gateway security

--- source/security/aether_security_lab.h
+++ source/security/aether_security_lab.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <nds.h>
-namespace aether::security {
+namespace aether::securitylab {
 enum Mode : u8 { PASSIVE_RF=0, AUTH_NETWORK=1, LAB_SIMULATION=2, GATEWAY_HARDENING=3, MODE_COUNT };
 struct Report { Mode mode; bool consent; bool txLocked; bool credentialCaptureLocked; bool destructiveLocked; u32 samples; u32 devices; u32 packets; u32 alerts; u32 labRuns; };
 void init(); void tick(); void setMode(Mode); void acknowledge(); void sample(); void analyze(); void runLabSimulation(); Report report(); const char* modeName(Mode); const char* warning();
