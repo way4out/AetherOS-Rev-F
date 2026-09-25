f91a6ef75996eb6f0e3b5867a0b6541d441c63e6
way4out <way_out4@yahoo.com>
2026-09-25 12:23:48 -0700

    Rev:F Phase 5: make HIL score reflect gateway readiness

--- source/core/hil.cpp
+++ source/core/hil.cpp
@@ -5,7 +5,7 @@
 #include "../audio/aether_audio.h"
 namespace { aether::hil::Report r={false,false,false,false,false,0}; unsigned t=0; }
 namespace aether::hil {
-void init(){r.protocol=protocol::selfTest();r.storage=hardware::sdAvailable();r.quantum=true;r.audio=true;r.gateways=false;r.score=(r.protocol?25:0)+(r.storage?25:0)+25+25;t=0;}
+void init(){r.protocol=protocol::selfTest();r.storage=hardware::sdAvailable();r.quantum=true;r.audio=true;r.gateways=false;r.score=(r.protocol?30:0)+(r.storage?25:0)+25;t=0;}
 void tick(){if(++t==60)r.gateways=true;}
 Report report(){return r;}
 }
