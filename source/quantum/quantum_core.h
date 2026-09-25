7159ddfdfe9e3ed6ddd9add9ae6cecf5b273035a
way4out <way_out4@yahoo.com>
2026-09-24 21:39:25 -0700

    Rev:F subsystem upgrade

--- source/quantum/quantum_core.h
+++ source/quantum/quantum_core.h
@@ -1,23 +1,7 @@
 #pragma once
 #include <nds.h>
-
 namespace aether::quantum {
 struct BackendInfo { const char* name; bool local; bool externalCapable; };
-struct Simulator {
-    int qubits;
-    int shots;
-    int lastMeasurement;
-    float probability[8];
-    bool bellState;
-};
-BackendInfo backend();
-void init(Simulator& q);
-void reset(Simulator& q);
-void hadamard(Simulator& q, int qbit);
-void pauliX(Simulator& q, int qbit);
-void cnot(Simulator& q, int control, int target);
-void runBell(Simulator& q);
-void runGrover2(Simulator& q);
-int measure(Simulator& q);
-void tick(Simulator& q);
+struct Simulator { int qubits; int shots; int lastMeasurement; float probability[8]; bool bellState; int algorithm; };
+BackendInfo backend(); void init(Simulator&); void reset(Simulator&); void setQubits(Simulator&,int); void hadamard(Simulator&,int); void pauliX(Simulator&,int); void cnot(Simulator&,int,int); void runBell(Simulator&); void runGrover2(Simulator&); void runDeutschJozsa(Simulator&); void runQFT2(Simulator&); void runTeleportation(Simulator&); int measure(Simulator&); void tick(Simulator&);
 }
