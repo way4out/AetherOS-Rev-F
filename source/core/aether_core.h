71641411ae1dbcf811aad0fc67aeaa04634e9d8c
way4out <way_out4@yahoo.com>
2026-09-24 21:42:27 -0700

    Expose Rev:F gateway state

--- source/core/aether_core.h
+++ source/core/aether_core.h
@@ -5,7 +5,7 @@ namespace aether {
 struct SystemState {
     bool sdReady; bool touchReady; bool benchmarkComplete; bool safeMode;
     bool sdWriteReady; bool audioReady; bool quantumReady; bool projectSaved;
-    u32 frame; int selectedModule; int screen;
+    u32 frame; int selectedModule; int screen; bool networkReady; bool gatewayConfigured;
 };
 void init(SystemState&);
 void update(SystemState&);
