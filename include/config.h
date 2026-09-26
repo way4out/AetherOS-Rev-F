d9e8a4bc489da606b4a1f928170b3478a62b0acc
way4out <way_out4@yahoo.com>
2026-09-25 20:38:07 -0700

    AetherMod 4 Pass 1: version foundation

--- include/config.h
+++ include/config.h
@@ -1,5 +1,5 @@
 #pragma once
 #define APP_NAME "AetherMod"
-#define APP_VERSION "AetherMod Q1"
+#define APP_VERSION "AetherMod 4.1"
 #define SAVE_MAGIC 0x414D4F44u
-#define SAVE_VERSION 2
+#define SAVE_VERSION 3
