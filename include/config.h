cd96e122fcd99b4f49201d214801c781ca1e2025
way4out <way_out4@yahoo.com>
2026-09-25 17:38:15 -0700

    Update FunCore identity

--- include/config.h
+++ include/config.h
@@ -1,5 +1,5 @@
 #pragma once
-#define APP_NAME "O2S2HaHa"
-#define APP_VERSION "NG-1.0"
+#define APP_NAME "AetherOS O2S2HaHa FunCore"
+#define APP_VERSION "NG-1.1"
 #define SAVE_MAGIC 0x4F325332u
 #define SAVE_VERSION 1
