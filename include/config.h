262b3018ed2fc7b22b0b489cfeab5e76e01e1236
way4out <way_out4@yahoo.com>
2026-09-25 17:50:17 -0700

    Rename AetherOSQ identity

--- include/config.h
+++ include/config.h
@@ -1,5 +1,5 @@
 #pragma once
-#define APP_NAME "AetherOS O2S2HaHa FunCore"
-#define APP_VERSION "NG-1.1"
-#define SAVE_MAGIC 0x4F325332u
+#define APP_NAME "Quantum AetherOSQ"
+#define APP_VERSION "Q1.0"
+#define SAVE_MAGIC 0x51414551u
 #define SAVE_VERSION 1
